ddfae30e0beda667259e5c4dbf9b71d52f437a2b
wuyah <lussiouserror@gmail.com>
2023-03-19 06:17:05 -0700

    second commit

---
+++ Day2/main.cpp
@@ -0,0 +1,22 @@
+#include "tgaimage.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+
+void line(int x0, int y0, int x1, int y1, TGAImage image, TGAColor color) {
+    for (int x=x0; x<=x1; x++) { 
+        float t = (x-x0)/(float)(x1-x0); 
+        int y = y0*(1.-t) + y1*t; 
+        image.set(x, y, color); 
+    } 
+}
+
+int main(int argc, char** argv) {
+	TGAImage image(100, 100, TGAImage::RGB);
+	TGAColor color('R', 1);
+	line(1, 1, 50, 60, image, color);
+	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.write_tga_file("output.tga");
+	return 0;
+}
+
